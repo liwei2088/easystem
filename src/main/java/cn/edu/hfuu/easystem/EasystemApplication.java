bf1c42ed4348468583a7e6274f7817ddd3425d92
liwei2088 <liwei2088@sohu.com>
2020-06-25 08:45:47 +0800

    Initial commit

---
+++ src/main/java/cn/edu/hfuu/easystem/EasystemApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.hfuu.easystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasystemApplication.class, args);
+    }
+
+}
